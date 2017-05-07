fe9fa1a48a5c70d321b4363a305d90d595e2e688
zhangjie <zx.zhangjie@qq.com>
2017-05-07 08:38:30 +0800

    try modify bus plan

--- app/src/main/java/com/zhangjie/trip/activity/TripRouteActivity.java
+++ app/src/main/java/com/zhangjie/trip/activity/TripRouteActivity.java
@@ -6,17 +6,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.InfoWindow;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.model.LatLng;
 import com.baidu.mapapi.search.core.RouteLine;
 import com.baidu.mapapi.search.core.SearchResult;
+import com.baidu.mapapi.search.route.BikingRouteLine;
 import com.baidu.mapapi.search.route.BikingRoutePlanOption;
 import com.baidu.mapapi.search.route.BikingRouteResult;
+import com.baidu.mapapi.search.route.DrivingRouteLine;
 import com.baidu.mapapi.search.route.DrivingRoutePlanOption;
 import com.baidu.mapapi.search.route.DrivingRouteResult;
 import com.baidu.mapapi.search.route.IndoorRouteResult;
+import com.baidu.mapapi.search.route.MassTransitRouteLine;
 import com.baidu.mapapi.search.route.MassTransitRouteResult;
 import com.baidu.mapapi.search.route.OnGetRoutePlanResultListener;
 import com.baidu.mapapi.search.route.PlanNode;
@@ -24,6 +32,7 @@ import com.baidu.mapapi.search.route.RoutePlanSearch;
 import com.baidu.mapapi.search.route.TransitRouteLine;
 import com.baidu.mapapi.search.route.TransitRoutePlanOption;
 import com.baidu.mapapi.search.route.TransitRouteResult;
+import com.baidu.mapapi.search.route.WalkingRouteLine;
 import com.baidu.mapapi.search.route.WalkingRoutePlanOption;
 import com.baidu.mapapi.search.route.WalkingRouteResult;
 import com.zhangjie.trip.R;
@@ -46,6 +55,11 @@ public class TripRouteActivity extends AppCompatActivity implements OnGetRoutePl
     private RouteLine<TransitRouteLine.TransitStep> route;
     private int nodeIndex=-1;
     private TransitRouteOverlay routeOverlay;
+    private Button mBtnPre,mBtnNext;
+    private int nowSearchType;
+    private MassTransitRouteLine massroute;
+    private MassTransitRouteResult nowResultmass;
+    private TextView popupText;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -57,12 +71,124 @@ public class TripRouteActivity extends AppCompatActivity implements OnGetRoutePl
 
         setContentView(R.layout.activity_trip_route);
         myMap= (MapView) findViewById(R.id.my_map_view);
+        mBtnPre = (Button) findViewById(R.id.pre);
+        mBtnNext = (Button) findViewById(R.id.next);
         mBaiduMap=myMap.getMap();
-        stNode=PlanNode.withCityNameAndPlaceName("合肥",startPoint);
-        enNode=PlanNode.withCityNameAndPlaceName("合肥",endPoint);
+        stNode=PlanNode.withCityNameAndPlaceName("北京",startPoint);
+        enNode=PlanNode.withCityNameAndPlaceName("北京",endPoint);
 
         mSearch = RoutePlanSearch.newInstance();
         mSearch.setOnGetRoutePlanResultListener(this);
+    }
+
+    public void nodeClick(View v) {
+        LatLng nodeLocation = null;
+        String nodeTitle = null;
+        Object step = null;
+
+        if (nowSearchType != 0 && nowSearchType != -1) {
+            // 非跨城综合交通
+            if (route == null || route.getAllStep() == null) {
+                return;
+            }
+            if (nodeIndex == -1 && v.getId() == R.id.pre) {
+                return;
+            }
+            // 设置节点索引
+            if (v.getId() == R.id.next) {
+                if (nodeIndex < route.getAllStep().size() - 1) {
+                    nodeIndex++;
+                } else {
+                    return;
+                }
+            } else if (v.getId() == R.id.pre) {
+                if (nodeIndex > 0) {
+                    nodeIndex--;
+                } else {
+                    return;
+                }
+            }
+            // 获取节结果信息
+            step = route.getAllStep().get(nodeIndex);
+            if (step instanceof DrivingRouteLine.DrivingStep) {
+                nodeLocation = ((DrivingRouteLine.DrivingStep) step).getEntrance().getLocation();
+                nodeTitle = ((DrivingRouteLine.DrivingStep) step).getInstructions();
+            } else if (step instanceof WalkingRouteLine.WalkingStep) {
+                nodeLocation = ((WalkingRouteLine.WalkingStep) step).getEntrance().getLocation();
+                nodeTitle = ((WalkingRouteLine.WalkingStep) step).getInstructions();
+            } else if (step instanceof TransitRouteLine.TransitStep) {
+                nodeLocation = ((TransitRouteLine.TransitStep) step).getEntrance().getLocation();
+                nodeTitle = ((TransitRouteLine.TransitStep) step).getInstructions();
+            } else if (step instanceof BikingRouteLine.BikingStep) {
+                nodeLocation = ((BikingRouteLine.BikingStep) step).getEntrance().getLocation();
+                nodeTitle = ((BikingRouteLine.BikingStep) step).getInstructions();
+            }
+        } else if (nowSearchType == 0) {
+            // 跨城综合交通  综合跨城公交的结果判断方式不一样
+
+
+            if (massroute == null || massroute.getNewSteps() == null) {
+                return;
+            }
+            if (nodeIndex == -1 && v.getId() == R.id.pre) {
+                return;
+            }
+            boolean isSamecity = nowResultmass.getOrigin().getCityId() == nowResultmass.getDestination().getCityId();
+            int size = 0;
+            if (isSamecity) {
+                size = massroute.getNewSteps().size();
+            } else {
+                for (int i = 0; i < massroute.getNewSteps().size(); i++) {
+                    size += massroute.getNewSteps().get(i).size();
+                }
+            }
+
+            // 设置节点索引
+            if (v.getId() == R.id.next) {
+                if (nodeIndex < size - 1) {
+                    nodeIndex++;
+                } else {
+                    return;
+                }
+            } else if (v.getId() == R.id.pre) {
+                if (nodeIndex > 0) {
+                    nodeIndex--;
+                } else {
+                    return;
+                }
+            }
+            if (isSamecity) {
+                // 同城
+                step = massroute.getNewSteps().get(nodeIndex).get(0);
+            } else {
+                // 跨城
+                int num = 0;
+                for (int j = 0; j < massroute.getNewSteps().size(); j++) {
+                    num += massroute.getNewSteps().get(j).size();
+                    if (nodeIndex - num < 0) {
+                        int k = massroute.getNewSteps().get(j).size() + nodeIndex - num;
+                        step = massroute.getNewSteps().get(j).get(k);
+                        break;
+                    }
+                }
+            }
+
+            nodeLocation = ((MassTransitRouteLine.TransitStep) step).getStartLocation();
+            nodeTitle = ((MassTransitRouteLine.TransitStep) step).getInstructions();
+        }
+
+        if (nodeLocation == null || nodeTitle == null) {
+            return;
+        }
+
+        // 移动节点至中心
+        mBaiduMap.setMapStatus(MapStatusUpdateFactory.newLatLng(nodeLocation));
+        // show popup
+        popupText = new TextView(TripRouteActivity.this);
+        popupText.setBackgroundResource(R.drawable.popup);
+        popupText.setTextColor(0xFF000000);
+        popupText.setText(nodeTitle);
+        mBaiduMap.showInfoWindow(new InfoWindow(popupText, nodeLocation, 0));
     }
 
     @Override
@@ -70,7 +196,7 @@ public class TripRouteActivity extends AppCompatActivity implements OnGetRoutePl
         super.onStart();
         if (method.equals("bus")){
             mSearch.transitSearch((new TransitRoutePlanOption())
-                    .from(stNode).city("合肥").to(enNode));
+                    .from(stNode).city("北京").to(enNode));
         }else if (method.equals("car")){
             mSearch.drivingSearch((new DrivingRoutePlanOption())
                     .from(stNode).to(enNode));
